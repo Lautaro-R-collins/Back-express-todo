e4fd26aae569e674464f856bb7d924ebcecebdb4
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-10-04 11:46:25 -0300

    commit cors front

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cors from "cors"
 
 dotenv.config();
-
 const app = express(); 
+
+app.use(cors( {
+  origin: "http://localhost:5173/"
+}))
 
 // middlewares
 app.use(express.json()); 
