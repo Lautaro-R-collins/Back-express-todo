c1e4a5134d4f06b97e8aade1a03d6b47378fe224
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-10-09 00:19:39 -0300

    add - board note model

--- src/models/noteModel.js
+++ src/models/noteModel.js
@@ -2,24 +2,13 @@ import mongoose from "mongoose";
 
 const noteSchema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    user: { 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: "User" 
-    },
+    title: { type: String, required: true },
+    content: { type: String },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    board: { type: mongoose.Schema.Types.ObjectId, ref: "Board" }, // <--- ESTE CAMPO
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 const Note = mongoose.model("Note", noteSchema);
-
 export default Note;
